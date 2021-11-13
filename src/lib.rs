6c12373bebe01281882238de85973fd179452049
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-11-12 20:11:40 -0500

    feat: cli

--- src/lib.rs
+++ src/lib.rs
@@ -2,44 +2,42 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
-	index: u32,
-	message: String,
+    index: u32,
+    message: String,
 }
 
-pub fn create_library(depth: u32) -> Vec<Entry> {
-	let characters = String::from_utf8(
-		(b'a'..=b'z')
-			.chain(b'A'..=b'Z')
-			.chain(b'0'..=b'9')
-			.chain(vec![b' ', b'-'])
-			.collect(),
-	)
-	.unwrap();
-	let mut library = vec![];
-	for char in characters.chars() {
-		library.push(Entry {
-			index: 0,
-			message: char.to_string(),
-		});
+pub fn create_library(depth: u32, uppercase: bool, digits: bool, symbols: bool) -> Vec<Entry> {
+    let characters = (b'a'..=b'z')
+        .chain(uppercase.then(|| b'A'..=b'Z').into_iter().flatten())
+        .chain(digits.then(|| b'0'..=b'9').into_iter().flatten())
+        .chain(uppercase.then(|| [b' ', b'-']).into_iter().flatten());
 
-		if depth > 1 {
-			let new_depth = depth - 1;
-			let mut new_library = create_library(new_depth);
-			for entry in new_library.iter_mut() {
-				entry.message.push(char);
-			}
-			library.append(&mut new_library);
-		}
-	}
+    let characters = String::from_utf8(characters.collect()).unwrap();
+    let mut library = vec![];
+    for char in characters.chars() {
+        library.push(Entry {
+            index: 0,
+            message: char.to_string(),
+        });
 
-	let final_library = library
-		.into_iter()
-		.enumerate()
-		.map(|(index, mut entry)| {
-			entry.index = index as u32 + 1;
-			entry
-		})
-		.collect();
+        if depth > 1 {
+            let new_depth = depth - 1;
+            let mut new_library = create_library(new_depth, uppercase, digits, symbols);
+            for entry in new_library.iter_mut() {
+                entry.message.push(char);
+            }
+            library.append(&mut new_library);
+        }
+    }
 
-	final_library
+    let final_library = library
+        .into_iter()
+        .enumerate()
+        .map(|(index, mut entry)| {
+            entry.index = index as u32 + 1;
+            entry
+        })
+        .collect();
+
+    final_library
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use std::{env, fs::File, io::BufWriter};
+use std::{fs::File, io::BufWriter};
+use structopt::StructOpt;
 
 mod lib;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+#[derive(StructOpt)]
+#[structopt(name = "lob", about = "Library of Babel")]
+struct Cli {
+    /// The max amount of letters in a word
+    depth: u32,
 
-    let library = lib::create_library(args[1].parse().expect("Invalid depth"));
+    /// Use uppercase letters
+    #[structopt(long, short = "u")]
+    uppercase: bool,
+
+    /// Use digits
+    #[structopt(long, short = "d")]
+    digits: bool,
+
+    /// Use symbols
+    #[structopt(long, short = "s")]
+    symbols: bool,
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+    let library = lib::create_library(args.depth, args.uppercase, args.digits, args.symbols);
     let file = File::create("library.json").unwrap();
 
     serde_json::to_writer(BufWriter::new(file), &library).unwrap();
